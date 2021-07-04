1b786135d850b0d5d36b5a6e7a6a410f14434d9c
AnnleeFores <annleefores@gmail.com>
2021-07-05 01:48:35 +0530

    main update

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from time import sleep
 from datetime import *
 import subprocess
 import os
+import schedule
 
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
@@ -21,18 +22,31 @@ pump.direction = digitalio.Direction.OUTPUT
 
 subprocess.call('alsactl --file ./audio_sensor_scripts/asound.state restore', shell=True)
 
-while True:
-    if chan.value < 10000:
-        print('Sensor value =',chan.value)
-    else: 
-        print('Sensor value =',chan.value)
+
+
+def printchan():
+    print('Sensor value =',chan.value)
+
+
+def micpi():
+    if chan.value > 10000:
         print('')
-        print('Running pump for 3sec')
+        subprocess.call('arecord --format=S16_LE --rate=16000 --file-type=wav --duration=10 --use-strftime %d-%m-%Y/%H-%M-%v.wav', shell=True)
+        print('')
+
+def pumppi():
+    if chan.value > 10000:
+        print('')
+        print('Running pump for 5 sec')
         pump.value = True
         sleep(5)
         pump.value = False
         print('')
-        subprocess.call('arecord --format=S16_LE --rate=16000 --file-type=wav --duration=10 --use-strftime %d-%m-%Y/%H-%M-%v.wav', shell=True)
-        print('')
-        sleep(2)
-    sleep(1.5)
+
+schedule.every(5).seconds.do(printchan)
+schedule.every(20).seconds.do(micpi)
+schedule.every().minute.at(":35").do(pumppi)
+
+while True:
+    schedule.run_pending()
+    sleep(1)
